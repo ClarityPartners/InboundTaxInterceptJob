de6854a729eb3ad364721fd4eca2c5f1a5fdc5df
Ryan DeSalvio <rdesalvi@purdue.edu>
2019-03-27 09:39:06 -0500

    Initial push of project.

---
+++ InboundTaxInterceptJob/InboundTaxInterceptJob/ApiJobTemplate/JobTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Tyler.Odyssey.JobProcessing;
+
+namespace TaxInterceptJob
+{
+  [ClassInterface(ClassInterfaceType.None)]
+  [Guid("c9045ff6-0d2f-490e-8a40-cda2ed783f2d")]
+  [ComVisible(true)]
+  public class JobTask : Task
+  {
+    protected override void SetupProcessor(string SiteID, string JobTaskXML)
+    {
+      Processor = new DataProcessor(SiteID, JobTaskXML);
+
+      ((DataProcessor)Processor).TaskParms = this.jobTaskParms;
+      ((DataProcessor)Processor).TaskUtility = this.taskUtility;
+      ((DataProcessor)Processor).TaskDocument = this.taskDocument;
+
+      UserID = ((DataProcessor)Processor).Context.UserID;
+    }
+
+    private int UserID { get; set; }
+  }
+}
